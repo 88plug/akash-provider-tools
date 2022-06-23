cb5a27415a81bf9d157750d7090cea0ab0ce1133
88plug <19512127+88plug@users.noreply.github.com>
2022-06-22 22:05:54 -0700

    Update start-akash.sh

--- start-akash.sh
+++ start-akash.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /home/akash
-. variables
 if [ -f ./microk8s-bootstrap.sh ]; then
   rm ./microk8s-bootstrap.sh
 fi
@@ -8,6 +7,7 @@ if [ ! -f variables ]; then
 wget -q https://raw.githubusercontent.com/88plug/akash-provider-tools/main/microk8s-bootstrap.sh
 chmod +x microk8s-bootstrap.sh ; echo "Enter your password to start the Akash installer" ; sudo ./microk8s-bootstrap.sh
 else
+. variables
 export KUBECONFIG=/home/akash/.kube/kubeconfig
 echo "Variables file deteted - Setup complete"
 fi
