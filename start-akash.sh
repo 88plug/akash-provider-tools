fedf79d7ff407ed647e7d212cc04959d50651703
88plug <19512127+88plug@users.noreply.github.com>
2022-06-25 08:43:42 -0700

    Update start-akash.sh

--- start-akash.sh
+++ start-akash.sh
@@ -5,8 +5,34 @@ if [ -f ./microk8s-bootstrap.sh ]; then
 fi
 if [ ! -f variables ]; then
 clear
+
+while true
+do
+clear
+read -p "Which install should we use (k3s/microk8s/kubespray)? (microk8s) : " METHOD_
+read -p "Are you sure you want to install with the $METHOD_ method? (y/n) " choice
+case "$choice" in
+  microk8s ) break;;
+  kubespray ) break;;
+  k3s ) echo "Not supported yet!  Please choose another option" ; sleep 3;;
+  * ) echo "Invalid entry, please try again with microk8s if unsure" ; sleep 3;;
+esac
+done
+
+if [[ $METHOD_ == "kubespray" ]]; then
+wget -q https://raw.githubusercontent.com/88plug/akash-provider-tools/main/kubespray-bootstrap.sh
+chmod +x kubespray-bootstrap.sh ; echo "No setup detected! Enter the default password 'akash' to start the Akash installer" ; sudo ./kubespray-bootstrap.sh
+fi
+if [[ $METHOD_ == "microk8s" ]]; then
 wget -q https://raw.githubusercontent.com/88plug/akash-provider-tools/main/microk8s-bootstrap.sh
 chmod +x microk8s-bootstrap.sh ; echo "No setup detected! Enter the default password 'akash' to start the Akash installer" ; sudo ./microk8s-bootstrap.sh
+fi
+if [[ $METHOD_ == "k3s" ]]; then
+wget -q https://raw.githubusercontent.com/88plug/akash-provider-tools/main/k3s-bootstrap.sh
+chmod +x microk8s-bootstrap.sh ; echo "No setup detected! Enter the default password 'akash' to start the Akash installer" ; sudo ./k3s-bootstrap.sh
+fi
+
+
 else
 . variables
 export KUBECONFIG=/home/akash/.kube/kubeconfig
