59928f56baf78d1fab662006c36ba4940d4ca373
88plug <19512127+88plug@users.noreply.github.com>
2022-06-25 08:59:36 -0700

    Update start-akash.sh

--- start-akash.sh
+++ start-akash.sh
@@ -19,15 +19,15 @@ esac
 done
 
 if [[ $METHOD_ == "kubespray" ]]; then
-wget -q https://raw.githubusercontent.com/88plug/akash-provider-tools/main/kubespray-bootstrap.sh
+curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/88plug/akash-provider-tools/main/kubespray-bootstrap.sh
 chmod +x kubespray-bootstrap.sh ; echo "No setup detected! Enter the default password 'akash' to start the Akash installer" ; sudo ./kubespray-bootstrap.sh
 fi
 if [[ $METHOD_ == "microk8s" ]]; then
-wget -q https://raw.githubusercontent.com/88plug/akash-provider-tools/main/microk8s-bootstrap.sh
+curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/88plug/akash-provider-tools/main/microk8s-bootstrap.sh
 chmod +x microk8s-bootstrap.sh ; echo "No setup detected! Enter the default password 'akash' to start the Akash installer" ; sudo ./microk8s-bootstrap.sh
 fi
 if [[ $METHOD_ == "k3s" ]]; then
-wget -q https://raw.githubusercontent.com/88plug/akash-provider-tools/main/k3s-bootstrap.sh
+curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/88plug/akash-provider-tools/main/k3s-bootstrap.sh
 chmod +x microk8s-bootstrap.sh ; echo "No setup detected! Enter the default password 'akash' to start the Akash installer" ; sudo ./k3s-bootstrap.sh
 fi
 
