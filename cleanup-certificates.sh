f6411cd6dfcad1ace5dade6a483645b504c08acc
88plug <19512127+88plug@users.noreply.github.com>
2023-02-25 02:15:31 -0800

    Update cleanup-certificates.sh

--- cleanup-certificates.sh
+++ cleanup-certificates.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
-PASSWORD="xxx"
+# Set the number of results to retrieve per page
+PAGE_SIZE=25
 
-echo $PASSWORD > key-pass.txt
+# Initialize the starting page number
+PAGE_NUM=1
 
-# Get the list of valid certificates owned by the account
-CERT_LIST=$(akash query cert list --owner $AKASH_ACCOUNT_ADDRESS --state valid -o json | jq '.certificates')
+# Loop until all results have been retrieved
+while true; do
+  # Get the next page of results
+  PAGE_RESULTS=$(akash query cert list --owner $AKASH_ACCOUNT_ADDRESS --node http://rpc.akash.rocks:26657 --state valid --page $PAGE_NUM --limit $PAGE_SIZE -o json)
 
-# Loop through the certificate list
-for CERT in $(echo "${CERT_LIST}" | jq -r '.[] | @base64'); do
-  # Decode the certificate JSON
-  CERT_JSON=$(echo ${CERT} | base64 --decode | jq -r '.')
+  # Check if there are no more results
+  if [[ $(echo $PAGE_RESULTS | jq '.certificates | length') -eq 0 ]]; then
+    break
+  fi
 
-  # Extract the certificate ID and domain
-  CERT_ID=$(echo "${CERT_JSON}" | jq -r '.serial')
+  # Loop through the page results
+  for CERT in $(echo "${PAGE_RESULTS}" | jq -r '.certificates[] | @base64'); do
+    # Decode the certificate JSON
+    CERT_JSON=$(echo ${CERT} | base64 --decode | jq -r '.')
 
-  # Do something with the certificate ID and domain
-  echo "Certificate ID: ${CERT_ID}"
+    # Extract the certificate ID and domain
+    CERT_ID=$(echo "${CERT_JSON}" | jq -r '.serial')
 
-  ( sleep 2s; cat key-pass.txt; cat key-pass.txt ) | akash tx cert revoke server --from $AKASH_ACCOUNT_ADDRESS --serial $CERT_ID -y -b async
+    # Do something with the certificate ID and domain
+    echo "Certificate ID: ${CERT_ID}"
+    ( sleep 2s; cat key-pass.txt; cat key-pass.txt ) | akash tx cert revoke server --from $AKASH_ACCOUNT_ADDRESS --serial $CERT_ID -y -b async
+    exit
+  done
 
+  # Increment the page number
+  PAGE_NUM=$((PAGE_NUM + 1))
 done
