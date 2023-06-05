9394fba7e54f1885125cf55775cd2169f5dfd75c
88plug <19512127+88plug@users.noreply.github.com>
2023-06-04 21:34:50 -0700

    Update start-akash.sh
    ChatGPT refactor original code

--- start-akash.sh
+++ start-akash.sh
@@ -1,44 +1,74 @@
 #!/bin/bash
+
 cd /home/akash
-if [ -f ./*bootstrap.sh ] ; then
-  echo "Found an old bootstrap - cleaning up"
-  rm ./microk8s-bootstrap.sh ; rm ./k3s-bootstrap.sh ; rm ./kubespray-bootstrap.sh
-fi
 
-if [ ! -f variables ]; then
-clear
+cleanup_bootstrap() {
+    if [ -f ./*bootstrap.sh ]; then
+        echo "Found an old bootstrap - cleaning up"
+        rm ./microk8s-bootstrap.sh
+        rm ./k3s-bootstrap.sh
+        rm ./kubespray-bootstrap.sh
+    fi
+}
 
-while true
-  do
-    clear
-    read -p "Which Kubernetes install method would you like to use (k3s/microk8s/kubespray)? (microk8s) : " METHOD_
-    read -p "Are you sure you want to install with the $METHOD_ method? (y/n) " choice
-    case "$choice" in
-      y|Y ) break;;
-      n|N ) echo "Please try again with microk8s if unsure" ; sleep 3;;
-      * ) echo "Invalid entry, please try again with Y or N" ; sleep 3;;
-  esac
-done
+run_bootstrap() {
+    local method=$1
+    local bootstrap_script
 
-if [[ $METHOD_ == "kubespray" ]]; then
-  wget -q --no-cache https://raw.githubusercontent.com/88plug/akash-provider-tools/main/kubespray-bootstrap.sh ; chmod +x kubespray-bootstrap.sh
-  echo "No setup detected! Enter the default password 'akash' to start the Akash installer" ; sudo ./kubespray-bootstrap.sh
-fi
-if [[ $METHOD_ == "microk8s" ]]; then
-  wget -q --no-cache https://raw.githubusercontent.com/88plug/akash-provider-tools/main/microk8s-bootstrap.sh ; chmod +x microk8s-bootstrap.sh ; 
-  echo "No setup detected! Enter the default password 'akash' to start the Akash installer" ; sudo ./microk8s-bootstrap.sh
-fi
-if [[ $METHOD_ == "k3s" ]]; then
-  wget -q --no-cache https://raw.githubusercontent.com/88plug/akash-provider-tools/main/k3s-bootstrap.sh ; chmod +x k3s-bootstrap.sh 
-  echo "No setup detected! Enter the default password 'akash' to start the Akash installer" ; sudo ./k3s-bootstrap.sh
-fi
+    case "$method" in
+        kubespray)
+            bootstrap_script="kubespray-bootstrap.sh"
+            ;;
+        microk8s)
+            bootstrap_script="microk8s-bootstrap.sh"
+            ;;
+        k3s)
+            bootstrap_script="k3s-bootstrap.sh"
+            ;;
+        *)
+            echo "Invalid method: $method"
+            exit 1
+            ;;
+    esac
 
-else
-. variables
+    wget -q --no-cache "https://raw.githubusercontent.com/88plug/akash-provider-tools/main/$bootstrap_script"
+    chmod +x "$bootstrap_script"
+    echo "No setup detected! Enter the default password 'akash' to start the Akash installer"
+    sudo "./$bootstrap_script"
+}
 
-if [[ $SETUP_COMPLETE == true ]]; then
-export KUBECONFIG=/home/akash/.kube/kubeconfig
-echo "Variables file deteted - Setup complete"
-fi
+main() {
+    cleanup_bootstrap
 
-fi
+    if [ ! -f variables ]; then
+        while true; do
+            read -p "Which Kubernetes install method would you like to use (k3s/microk8s/kubespray)? (microk8s): " method
+            read -p "Are you sure you want to install with the $method method? (y/n): " choice
+
+            case "$choice" in
+                [Yy])
+                    run_bootstrap "$method"
+                    break
+                    ;;
+                [Nn])
+                    echo "Please try again with microk8s if unsure"
+                    sleep 3
+                    ;;
+                *)
+                    echo "Invalid entry, please try again with Y or N"
+                    sleep 3
+                    ;;
+            esac
+        done
+    else
+        source variables
+
+        if [[ $SETUP_COMPLETE == true ]]; then
+            export KUBECONFIG=/home/akash/.kube/kubeconfig
+            echo "Variables file detected - Setup complete"
+        fi
+    fi
+}
+
+# Execute the main function
+main
