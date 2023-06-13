acb88814796e95b1024f3fd81818d574637ed774
88plug <19512127+88plug@users.noreply.github.com>
2023-06-13 04:59:54 -0700

    Update start-akash.sh

--- start-akash.sh
+++ start-akash.sh
@@ -5,15 +5,15 @@ cd /home/akash
 if [ -f variables ]; then
 #Only run GPU if variables found, after first setup.
 function configure_gpu() {
-  echo "Detected GPU but not set up. Starting configuration..."
+echo "Detected GPU but not set up. Starting configuration..."
 
-  # Add Helm repositories
-  helm repo add nvdp https://nvidia.github.io/k8s-device-plugin
-  helm repo add akash https://akash-network.github.io/helm-charts
-  helm repo update
+# Add Helm repositories
+helm repo add nvdp https://nvidia.github.io/k8s-device-plugin
+helm repo add akash https://akash-network.github.io/helm-charts
+helm repo update
 
-  # Create NVIDIA RuntimeClass
-  cat > /home/akash/gpu-nvidia-runtime-class.yaml <<EOF
+# Create the NVIDIA Runtime Config
+cat > nvidia-runtime-class.yaml << EOF
 kind: RuntimeClass
 apiVersion: node.k8s.io/v1
 metadata:
@@ -21,20 +21,21 @@ metadata:
 handler: nvidia
 EOF
 
-  kubectl apply -f /home/akash/gpu-nvidia-runtime-class.yaml
+kubectl apply -f /home/akash/gpu-nvidia-runtime-class.yaml
 
-  # Install NVIDIA Device Plugin
-  helm upgrade -i nvdp nvdp/nvidia-device-plugin \
-    --namespace nvidia-device-plugin \
-    --create-namespace \
-    --set runtimeClassName="nvidia"
+# Apply the NVIDIA Runtime Config
 
-  echo "Waiting 10 seconds for the GPU to settle..."
-  sleep 10
-  kubectl get pods -A -o wide
+helm upgrade -i nvdp nvdp/nvidia-device-plugin \
+  --namespace nvidia-device-plugin \
+  --create-namespace \
+  --version 0.13.0 \
+  --set runtimeClassName="nvidia"
 
-  # Set GPU_ENABLED to true
-  echo "GPU_ENABLED=true" >> variables
+echo "Waiting 30 seconds for the GPU to settle..."
+sleep 30
+kubectl get pods -A -o wide
+# Set GPU_ENABLED to true
+echo "GPU_ENABLED=true" >> variables
 }
 
 function create_test_pod() {
@@ -43,12 +44,6 @@ export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 fi
 
 cat > gpu-test-pod.yaml << EOF
-apiVersion: node.k8s.io/v1
-kind: RuntimeClass
-metadata:
-  name: nvidia
-handler: nvidia
----
 apiVersion: v1
 kind: Pod
 metadata:
@@ -71,11 +66,11 @@ spec:
       value: all      
 EOF
 
-  kubectl apply -f gpu-test-pod.yaml
-  echo "Waiting 10 seconds for the test pod to start..."
-  sleep 10
-  kubectl logs nbody-gpu-benchmark
-  kubectl get pods -A -o wide
+kubectl apply -f gpu-test-pod.yaml
+echo "Waiting 10 seconds for the test pod to start..."
+sleep 10
+kubectl logs nbody-gpu-benchmark
+kubectl get pods -A -o wide
 }
 
 
