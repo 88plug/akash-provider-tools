d92b0497b3bf93e6f3047951fa6935820f826b4f
88plug <19512127+88plug@users.noreply.github.com>
2023-06-12 17:35:26 -0700

    Update start-akash.sh

--- start-akash.sh
+++ start-akash.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
 cd /home/akash
+. variables
+
+if lspci | grep -q NVIDIA && $GPU_ENABLED != "true"; then
+function gpu(){
+helm repo add nvdp https://nvidia.github.io/k8s-device-plugin
+helm repo add akash https://akash-network.github.io/helm-charts
+helm repo update
+cat > /home/akash/gpu-nvidia-runtime-class.yaml << EOF
+kind: RuntimeClass
+apiVersion: node.k8s.io/v1
+metadata:
+  name: nvidia
+handler: nvidia
+EOF
+kubectl apply -f /home/akash/gpu-nvidia-runtime-class.yaml
+helm upgrade -i nvdp nvdp/nvidia-device-plugin \
+  --namespace nvidia-device-plugin \
+  --create-namespace \
+  --version 0.13.0 \
+  --set runtimeClassName="nvidia"
+
+echo "Waiting 30 seconds for GPU to settle..."
+sleep 30
+echo "GPU_ENABLED=true" >> variables
+}
+gpu
+fi
+
 
 cleanup_bootstrap() {
     if [ -f ./*bootstrap.sh ]; then
