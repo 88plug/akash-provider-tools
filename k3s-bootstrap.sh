4693743804675426f9a1e6d3da4e3571e62694bc
88plug <19512127+88plug@users.noreply.github.com>
2023-06-12 22:32:58 -0700

    Update k3s-bootstrap.sh

--- k3s-bootstrap.sh
+++ k3s-bootstrap.sh
@@ -43,8 +43,49 @@ case "$choice" in
 esac
 done
 
+#Dynamic or Static Public IP?
+while true
+do
+clear
+read -p "Do you have a dynamic or static IP address? : $ip_ (dynamic/static)? " choice
+case "$choice" in
+  dynamic|DYNAMIC ) echo "You chose dynamic IP" ; ip_=dynamic ; break;;
+  static|STATIC ) echo "You chose static" ;  ip_=static ; break;;
+  * ) echo "Invalid entry, please try again with dynamic or static";;
+esac
+done 
 
+if [[ $ip_ == "dynamic" ]]; then
+echo "Dynamic IP Detected"
+  echo "You must use a Dynamic DNS / No-IP service."
+    while true
+    do
+    clear
+    read -p "Enter your dynamic DNS url (akash.no-ip.com) : " DYNAMICIP_
+    read -p "Are you sure the dynamic DNS url is correct? : $DYNAMICIP_ (y/n)? " choice
+    case "$choice" in
+      y|Y ) break;;
+      n|N ) echo "Try again" ; sleep 3;;
+      * ) echo "Invalid entry, please try again with Y or N" ; sleep 3;;
+    esac
+    done
+cat <<EOF > ./dns-records.txt
+*.ingress 300 IN CNAME nodes.$DOMAIN_.
+nodes 300 IN CNAME $DYNAMICIP_.
+provider 300 IN CNAME nodes.$DOMAIN_.
+rpc 300 IN CNAME nodes.$DOMAIN_.
+EOF
+else
+cat <<EOF > ./dns-records.txt
+*.ingress 300 IN CNAME nodes.$DOMAIN_.
+nodes 300 IN A X.X.X.X. #IP of this machine and any additional nodes
+nodes 300 IN A X.X.X.X. #IP of any additional nodes
+nodes 300 IN A X.X.X.X. #IP of any additional nodes
+provider 300 IN CNAME nodes.$DOMAIN_.
+rpc 300 IN CNAME nodes.$DOMAIN_.
+EOF
 
+fi
 
 #read -p "Enter domain name to use for your provider (example.com) : " DOMAIN_
 #read -p "Enter mnemonic phrase to import your provider wallet (KING SKI GOAT...): " mnemonic_
@@ -57,10 +98,21 @@ done
 #Store securely for user
 KEY_SECRET_=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
 
-
+function depends(){
 export DEBIAN_FRONTEND=noninteractive
 apt-get update && apt-get dist-upgrade -yqq
 snap install kubectl --classic ; snap install helm --classic
+#Disable sleep
+systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+#Disable IPv6
+sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="maybe-ubiquity"/GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 maybe-ubiquity"/' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+#Fast reboots
+sed -i -e 's/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=5s/' /etc/systemd/system.conf
+systemctl daemon-reload
+}
+depends &>> /home/akash/logs/installer/depends.log
+
 
 function gpu() {
     if lspci | grep -q NVIDIA; then
@@ -127,17 +179,6 @@ sleep 30
 echo "Checking cluster is up..."
 kubectl get pods -A -o wide
 
-function depends(){
-#Disable sleep
-systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
-#Disable IPv6
-sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="maybe-ubiquity"/GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 maybe-ubiquity"/' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
-#Fast reboots
-sed -i -e 's/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=5s/' /etc/systemd/system.conf
-systemctl daemon-reload
-}
-depends
 
 function install_akash(){
 #Install Akash and setup wallet
@@ -212,56 +253,16 @@ chmod +x run-helm-microk8s.sh ; chmod +x bid-engine-script.sh
 chown akash:akash *.sh
 
 ./run-helm-microk8s.sh
- 
-while true
-do
-clear
-read -p "Do you have a dynamic or static IP address? : $ip_ (dynamic/static)? " choice
-case "$choice" in
-  dynamic|DYNAMIC ) echo "You chose dynamic IP" ; ip_=dynamic ; break;;
-  static|STATIC ) echo "You chose static" ;  ip_=static ; break;;
-  * ) echo "Invalid entry, please try again with dynamic or static";;
-esac
-done 
 
-if [[ $ip_ == "dynamic" ]]; then
-echo "Dynamic IP Detected"
-  echo "You must use a Dynamic DNS / No-IP service."
-    while true
-    do
-    clear
-    read -p "Enter your dynamic DNS url (akash.no-ip.com) : " DYNAMICIP_
-    read -p "Are you sure the dynamic DNS url is correct? : $DYNAMICIP_ (y/n)? " choice
-    case "$choice" in
-      y|Y ) break;;
-      n|N ) echo "Try again" ; sleep 3;;
-      * ) echo "Invalid entry, please try again with Y or N" ; sleep 3;;
-    esac
-    done
-  echo "You must configure your DNS records to match this format and open the following ports"
-cat <<EOF > ./dns-records.txt
-*.ingress 300 IN CNAME nodes.$DOMAIN_.
-nodes 300 IN CNAME $DYNAMICIP_.
-provider 300 IN CNAME nodes.$DOMAIN_.
-rpc 300 IN CNAME nodes.$DOMAIN_.
-EOF
-  cat ./dns-records.txt
-else
-  echo "You must configure your DNS records to match this format and open the following ports"
-cat <<EOF > ./dns-records.txt
-*.ingress 300 IN CNAME nodes.$DOMAIN_.
-nodes 300 IN A X.X.X.X. #IP of this machine and any additional nodes
-nodes 300 IN A X.X.X.X. #IP of any additional nodes
-nodes 300 IN A X.X.X.X. #IP of any additional nodes
-provider 300 IN CNAME nodes.$DOMAIN_.
-rpc 300 IN CNAME nodes.$DOMAIN_.
-EOF
-  cat ./dns-records.txt
-fi
+rm -f microk8s-bootstrap.sh
+chown akash:akash *.sh
+chown akash:akash *.txt
+chown akash:akash variables
+
+#echo "WALLET_FUNDED=0" >> variables
 
 echo "Firewall Setup Required" 
 echo "Please forward these ports to the IP of this machine"
-
 cat <<EOF > ./firewall-ports.txt
 8443/tcp - for manifest uploads
 80/tcp - for web app deployments
@@ -269,15 +270,14 @@ cat <<EOF > ./firewall-ports.txt
 30000-32767/tcp - for Kubernetes node port range for deployments
 30000-32767/udp - for Kubernetes node port range for deployments
 EOF
-
 cat ./firewall-ports.txt
-rm -f microk8s-bootstrap.sh
-chown akash:akash *.sh
-chown akash:akash *.txt
-chown akash:akash variables
 
-#echo "WALLET_FUNDED=0" >> variables
+
+
 echo "SETUP_COMPLETE=true" >> variables
+
+
+
 
 echo "Setup Complete"
 echo "Rebooting in 10 seconds..."
