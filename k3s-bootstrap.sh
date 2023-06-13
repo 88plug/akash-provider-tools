63356653d10b12e86408d446b9d8655cf5777031
88plug <19512127+88plug@users.noreply.github.com>
2023-06-13 05:43:35 -0700

    Update k3s-bootstrap.sh

--- k3s-bootstrap.sh
+++ k3s-bootstrap.sh
@@ -210,7 +210,7 @@ curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | apt-key add -
 curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | tee /etc/apt/sources.list.d/libnvidia-container.list
 apt-get update
 ubuntu-drivers install --gpgpu
-DEBIAN_FRONTEND=noninteractive apt-get install -y nvidia-cuda-toolkit nvidia-container-toolkit nvidia-container-runtime ubuntu-drivers-commons
+DEBIAN_FRONTEND=noninteractive apt-get install -y nvidia-cuda-toolkit nvidia-container-toolkit nvidia-container-runtime ubuntu-drivers-common
 grep nvidia /var/lib/rancher/k3s/agent/etc/containerd/config.toml
 fi
 }
