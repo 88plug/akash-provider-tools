5d29a9fe0c643c7f55140e7870c75c2576d24b37
88plug <19512127+88plug@users.noreply.github.com>
2023-06-13 05:02:10 -0700

    Update start-akash.sh

--- start-akash.sh
+++ start-akash.sh
@@ -3,6 +3,24 @@
 cd /home/akash
 
 if [ -f variables ]; then
+
+function gpu() {
+    if lspci | grep -q NVIDIA; then
+        distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+        curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | apt-key add - 
+        curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | tee /etc/apt/sources.list.d/libnvidia-container.list
+        apt-get update
+        ubuntu-drivers autoinstall
+        DEBIAN_FRONTEND=noninteractive apt-get install -y nvidia-cuda-toolkit nvidia-container-toolkit nvidia-container-runtime ubuntu-drivers-commons
+        # DEBIAN_FRONTEND=noninteractive apt-get install -y cuda-drivers-fabricmanager-515 
+    else
+        echo "No GPU Detected"
+    fi
+}
+echo "☸️ Installing GPU"
+gpu &>> /home/akash/logs/installer/gpu.log
+
+
 #Only run GPU if variables found, after first setup.
 function configure_gpu() {
 echo "Detected GPU but not set up. Starting configuration..."
