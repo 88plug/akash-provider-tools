494131aec0859053fc44357162eee3043823ef51
88plug <19512127+88plug@users.noreply.github.com>
2023-06-12 21:01:38 -0700

    Update k3s-bootstrap.sh

--- k3s-bootstrap.sh
+++ k3s-bootstrap.sh
@@ -68,10 +68,10 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | apt-key add -
 curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | tee /etc/apt/sources.list.d/libnvidia-container.list
 apt-get update
-ubuntu-drivers install --gpgpu
+ubuntu-drivers autoinstall
 DEBIAN_FRONTEND=noninteractive apt-get install -y nvidia-cuda-toolkit nvidia-container-toolkit nvidia-container-runtime ubuntu-drivers-commons
-# DEBIAN_FRONTEND=noninteractive apt-get install -y cuda-drivers-fabricmanager-525
 grep nvidia /var/lib/rancher/k3s/agent/etc/containerd/config.toml
+sleep 5
 else
 echo "No GPU Detected"
 fi
