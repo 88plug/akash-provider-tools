e60d6434e4591ddde21165ac562ac00a6b0d81e3
88plug <19512127+88plug@users.noreply.github.com>
2023-07-03 15:14:57 +0200

    Update k3s-bootstrap.sh

--- k3s-bootstrap.sh
+++ k3s-bootstrap.sh
@@ -217,7 +217,8 @@ chown akash:akash cilium
 mv cilium /usr/local/bin/
 rm -f cilium-linux-amd64.tar.gz
 
-cilium install --set kubeProxyReplacement=strict --set bandwidthManager.enabled=true --set global.containerRuntime.integration="containerd" --set global.containerRuntime.socketPath="/var/run/k3s/containerd/containerd.sock"
+# cilium install --set kubeProxyReplacement=strict --set bandwidthManager.enabled=true --set global.containerRuntime.integration="containerd" --set global.containerRuntime.socketPath="/var/run/k3s/containerd/containerd.sock"
+cilium install --set bandwidthManager.enabled=true
 
 #Very much needed!
 # cilium config set kube-proxy-replacement strict
