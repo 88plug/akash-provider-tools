3e30f1243aa11d925001543a13d0048d0458e80d
88plug <19512127+88plug@users.noreply.github.com>
2023-09-05 15:25:05 -0700

    Update gpu-power.sh

--- gpu-power.sh
+++ gpu-power.sh
@@ -15,13 +15,49 @@ for GPU_ID in $GPU_IDS; do
 
     # Determine power limit based on GPU model
     case "$GPU_NAME" in
+        *"950"*) POWER_LIMIT=75 ;;
         *"960"*) POWER_LIMIT=120 ;;
-        *"1660"*) POWER_LIMIT=120 ;;
+        *"970"*) POWER_LIMIT=145 ;;
+        *"980"*) POWER_LIMIT=165 ;;
+        *"980 Ti"*) POWER_LIMIT=250 ;;
+        *"1050"*) POWER_LIMIT=75 ;;
+        *"1050 Ti"*) POWER_LIMIT=75 ;;
+        *"1060"*) POWER_LIMIT=120 ;;
+        *"1070"*) POWER_LIMIT=150 ;;
+        *"1070 Ti"*) POWER_LIMIT=180 ;;
         *"1080"*) POWER_LIMIT=175 ;;
+        *"1080 Ti"*) POWER_LIMIT=250 ;;
+        
+        # 20 Series
+        *"1650"*) POWER_LIMIT=75 ;;
+        *"1660"*) POWER_LIMIT=120 ;;
+        *"1660 Ti"*) POWER_LIMIT=120 ;;
+        *"2060"*) POWER_LIMIT=160 ;;
         *"2070"*) POWER_LIMIT=145 ;;
+        *"2070 Super"*) POWER_LIMIT=175 ;;
+        *"2080"*) POWER_LIMIT=215 ;;
+        *"2080 Super"*) POWER_LIMIT=250 ;;
+        *"2080 Ti"*) POWER_LIMIT=260 ;;
+        
+        # 30 Series
+        *"3050"*) POWER_LIMIT=90 ;;
         *"3060"*) POWER_LIMIT=130 ;;
+        *"3060 Ti"*) POWER_LIMIT=150 ;;
         *"3070"*) POWER_LIMIT=165 ;;
+        *"3070 Ti"*) POWER_LIMIT=185 ;;
+        *"3080"*) POWER_LIMIT=235 ;;
+        *"3080 Ti"*) POWER_LIMIT=270 ;;
         *"3090"*) POWER_LIMIT=250 ;;
+        
+        # 40 Series
+        *"4050"*) POWER_LIMIT=100 ;;
+        *"4060"*) POWER_LIMIT=140 ;;
+        *"4060 Ti"*) POWER_LIMIT=160 ;;
+        *"4070"*) POWER_LIMIT=180 ;;
+        *"4070 Ti"*) POWER_LIMIT=200 ;;
+        *"4080"*) POWER_LIMIT=255 ;;
+        *"4080 Ti"*) POWER_LIMIT=280 ;;
+        *"4090"*) POWER_LIMIT=300 ;;
         *) echo "Unknown GPU model: $GPU_NAME"; exit 1 ;;
     esac
 
